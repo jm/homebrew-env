375d93ed2a04a22d4afdf5062470e8dfdd5a6d96
Jeremy McAnally <jeremymcanally@gmail.com>
2012-03-29 10:07:10 -0400

    Version

--- ruby-build-fork.rb
+++ ruby-build-fork.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class RubyBuildFork < Formula
   homepage 'https://github.com/Empact/ruby-build'
-  url 'https://github.com/Empact/ruby-build/tarball/master'
+  url 'https://github.com/Empact/ruby-build/tarball/v03292012'
   md5 'b14d247111c404c4576bda9a11ba0eb6'
 
   head 'https://github.com/Empact/ruby-build.git'
