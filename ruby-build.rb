77dc464f97211a8027f36c6da7a2bfba6b71b8a6
Jeremy McAnally <jeremymcanally@gmail.com>
2012-03-29 09:56:12 -0400

    Add forked version

---
+++ ruby-build.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class RubyBuild < Formula
+  homepage 'https://github.com/Empact/ruby-build'
+  url 'https://github.com/Empact/ruby-build/tarball/master'
+  md5 'b14d247111c404c4576bda9a11ba0eb6'
+
+  head 'https://github.com/Empact/ruby-build.git'
+
+  def install
+    ENV['PREFIX'] = prefix
+    system "./install.sh"
+  end
+end
