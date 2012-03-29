322996a3a773dab9dd05f41b7e57376bd3433f3a
Jeremy McAnally <jeremymcanally@gmail.com>
2012-03-29 10:07:47 -0400

    Switch username

--- ruby-build-fork.rb
+++ ruby-build-fork.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class RubyBuildFork < Formula
-  homepage 'https://github.com/Empact/ruby-build'
-  url 'https://github.com/Empact/ruby-build/tarball/v03292012'
+  homepage 'https://github.com/jm/ruby-build'
+  url 'https://github.com/jm/ruby-build/tarball/v03292012'
   md5 'b14d247111c404c4576bda9a11ba0eb6'
 
-  head 'https://github.com/Empact/ruby-build.git'
+  head 'https://github.com/jm/ruby-build.git'
 
   def install
     ENV['PREFIX'] = prefix
