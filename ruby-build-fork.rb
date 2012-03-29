3a8571d1eedb4ea14b0227703adc6bf4f80d6539
Jeremy McAnally <jeremymcanally@gmail.com>
2012-03-29 10:08:12 -0400

    Update hash

--- ruby-build-fork.rb
+++ ruby-build-fork.rb
@@ -3,7 +3,7 @@ require 'formula'
 class RubyBuildFork < Formula
   homepage 'https://github.com/jm/ruby-build'
   url 'https://github.com/jm/ruby-build/tarball/v03292012'
-  md5 'b14d247111c404c4576bda9a11ba0eb6'
+  md5 '5b558b404882861f92840749313655bb'
 
   head 'https://github.com/jm/ruby-build.git'
 
