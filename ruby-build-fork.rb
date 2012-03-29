5c46e0b84a86d07a20f8aaabc7626edb1d4ebb27
Jeremy McAnally <jeremymcanally@gmail.com>
2012-03-29 09:58:49 -0400

    Move so we can tap it

--- ruby-build-fork.rb
+++ ruby-build-fork.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class RubyBuild < Formula
+class RubyBuildFork < Formula
   homepage 'https://github.com/Empact/ruby-build'
   url 'https://github.com/Empact/ruby-build/tarball/master'
   md5 'b14d247111c404c4576bda9a11ba0eb6'
